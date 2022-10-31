dddc73ea205696a2693ce2bc037dfbdba3cecb1e
gustavosouza <gustavogss.dev@gmail.com>
2022-10-31 00:19:56 -0300

    Método POST - CREATABLE

--- endpoints/user_post.php
+++ endpoints/user_post.php
@@ -1,6 +1,20 @@
 <?php 
 
 function api_user_post($request) {
-   
+
+    $response = [
+        'ID' => '2',
+        'user_login' => 'gustavo souza'
+    ];
+
+    return rest_ensure_response($response);
 }
+
+function register_api_user_post(){
+    register_rest_route('api', '/user', [
+        'methods' => WP_REST_Server::CREATABLE,
+        'callback' => 'api_user_post',
+    ]);
+}
+add_action('rest_api_init', 'register_api_user_post');
 ?>
